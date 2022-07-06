42e44d560d062f73ce33b9d458c5582e211f2ff3
Matthew Bleathman <matthew@bleathman.com>
2022-07-06 22:58:09 +0300

    Code Structural and Comment Changes
    - Cleaned up SCSS and added descriptive comments
    - Amended HTML
    - Added descriptive comments to JS scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
+// Mobile Menu onClick open
 $(".mobile-nav-hamburger svg").click(function() {
+
     $('#main-navigation').toggleClass('open');
 
 });
 
+// Closes mobile navigation on resize
 $(window).on('resize', function() {
 
     if ($(window).width() <= 600) { 
-
+        
         $('#main-navigation').removeClass('open');
 
     } 
